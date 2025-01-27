936ed6c74b42ebf8007ce197ac7f63d533b1632b
Rafassz <rafassz.aps@gmail.com>
2025-01-27 11:31:11 -0300

    Bibliotecas e Componentes

---
+++ farmacia/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './pages/footer/Footer';
+import Home from './pages/home/Home';
+import Navbar from './pages/navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
